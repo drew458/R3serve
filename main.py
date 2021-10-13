2faa8fd45147b5ca80edcbeaddf2e138f774926b
drew <andry.marini@gmail.com>
2021-10-13 16:39:44 +0200

    added cmd argument for Automatic Course Reserve mode

--- main.py
+++ main.py
@@ -7,27 +7,6 @@ import CourseNames
 import Login
 import Worker
 
-"""def argumentEvaluation():
-    # create the top-level parser
-    parser = argparse.ArgumentParser(description='Enter the working mode of the script')
-    parser.add_argument("-M", "--manual", help="Manual mode", action="store_true")
-    subparsers = parser.add_subparsers(help='sub-command help')
-
-    # create the parser for the "a" command
-    parser_a = subparsers.add_parser('a', help='Automatic mode')
-    parser_a.add_argument('bar', type=int, help='bar help')
-
-    # Read arguments from the command line
-    args = parser.parse_args()
-
-    # Check for --automatic or -A
-    if args.automatic:
-        logging.info("Automatic mode selected...")
-
-    # Check for --manual or -M
-    if args.manual:
-        logging.info("Manual mode selected...") """
-
 
 def main():
     # argumentEvaluation()
@@ -45,28 +24,42 @@ def main():
     parser.add_argument("-C", "-c", "-course", "--course", default=None, type=str,
                         help="The course you want to reserve",
                         nargs="+", action=MyAction)
+    parser.add_argument("-A", "-a", "-auto", "-automatic", "--automatic", action="store_true",
+                        help="Run the program only in Automatic Course Reserve mode")
     parser.add_argument("-H", "-hl", "-headless", "--headless", action="store_true",
                         help="Don't open the browser for the whole task"
                              "(Headless mode)")
 
     args = parser.parse_args()
 
+    if args.automatic is True:
+        mainAutomatic(args.username, args.password, args.course, args.headless)
+    else:
+        mainManual(args.username, args.password, args.course, args.headless)
+
+
+def mainManual(inputUsername, inputPassword, inputCourse, isHeadless):
     # Initialize loggin format
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
     # Perform the log in
-    driver = Login.login(args.username, args.password, args.headless)
+    driver = Login.login(inputUsername, inputPassword, isHeadless)
 
     # Insert the course
-    selected_course = CourseNames.insertCourse(args.course)
-
-    # Start the automatic reserve thread
-    automatic_reservations = Thread(target=Automation.scheduler, args=(args.username, args.password,
-                                                                       args.headless))
-    automatic_reservations.start()
+    selected_course = CourseNames.insertCourse(inputCourse)
 
     # Reserve the lesson
     Worker.reserve(driver, selected_course)
+
+
+def mainAutomatic(inputUsername, inputPassword, inputCourse, isHeadless):
+    # Initialize loggin format
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+    # Start the automatic reserve thread
+    automatic_reservations = Thread(target=Automation.scheduler, args=(inputUsername, inputPassword,
+                                                                       isHeadless))
+    automatic_reservations.start()
 
 
 # Press the green button on the left side to run the script
