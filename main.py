1e773b455fd7bb372dc3f098b0a06ec758ef1aa7
drew <andry.marini@gmail.com>
2021-10-13 12:22:29 +0200

    command line argument init

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 from threading import Thread
 
@@ -7,7 +8,30 @@ import Login
 import Worker
 
 
+"""def argumentEvaluation():
+    # create the top-level parser
+    parser = argparse.ArgumentParser(description='Enter the working mode of the script')
+    parser.add_argument("-M", "--manual", help="Manual mode", action="store_true")
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    # create the parser for the "a" command
+    parser_a = subparsers.add_parser('a', help='Automatic mode')
+    parser_a.add_argument('bar', type=int, help='bar help')
+
+    # Read arguments from the command line
+    args = parser.parse_args()
+
+    # Check for --automatic or -A
+    if args.automatic:
+        logging.info("Automatic mode selected...")
+
+    # Check for --manual or -M
+    if args.manual:
+        logging.info("Manual mode selected...") """
+
+
 def main():
+    # argumentEvaluation()
 
     logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
