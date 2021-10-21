999607d91509cead55c3ba02022827b380ddcaea
drew <andry.marini@gmail.com>
2021-10-22 00:42:08 +0200

    init

---
+++ LoginWithWebsiteCookies.py
@@ -0,0 +1,47 @@
+import time
+
+import selenium.common.exceptions
+from selenium import webdriver
+import pickle
+
+import static.Cred as Cred
+
+
+def login():
+    try:
+        # Login using the stored cookies for automatic authentication:
+
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options)
+        driver.get('https://gomp.uniroma3.it/Login?ReturnUrl=%2f')
+        cookies = pickle.load(open("Resources/cookies.pkl", "rb"))
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+        driver.get('https://gomp.uniroma3.it/Login?ReturnUrl=%2f')
+    except Exception:
+        # Login and store the cookies
+
+        # options = webdriver.ChromeOptions()
+        # driver = webdriver.Chrome(options=options)
+        driver.get('https://gomp.uniroma3.it/Login?ReturnUrl=%2f')
+        driver.find_element_by_id("userName").send_keys(Cred.username)
+        driver.find_element_by_id("password").send_keys(Cred.password)
+        driver.find_element_by_id("loginButton").click()
+        pickle.dump(driver.get_cookies(), open("Resources/cookies.pkl", "wb"))
+
+    time.sleep(2)
+    try:
+        driver.find_element_by_id("userName")
+    except selenium.common.exceptions.NoSuchElementException:
+        return driver
+
+    # Login and store the cookies
+    # options = webdriver.ChromeOptions()
+    # driver = webdriver.Chrome(options=options)
+    driver.get('https://gomp.uniroma3.it/Login?ReturnUrl=%2f')
+    driver.find_element_by_id("userName").send_keys(Cred.username)
+    driver.find_element_by_id("password").send_keys(Cred.password)
+    driver.find_element_by_id("loginButton").click()
+    pickle.dump(driver.get_cookies(), open("Resources/cookies.pkl", "wb"))
+
+    return driver
